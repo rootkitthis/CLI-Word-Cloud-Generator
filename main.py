331d7eaee169deb948edbc529471de3060ea3820
Mike <heymister@mikemazade.com>
2025-01-24 16:01:41 -0600

    removed trailing spaces, and added some changes for readability in the main logic

--- main.py
+++ main.py
@@ -4,52 +4,53 @@ import matplotlib.pyplot as plt
 from collections import Counter
 import re
 
-#Important file paths stored as variables 
+#Important file paths stored as variables
 #note: change file paths to location of file paths locally on your machine
-file_path = ''
-excluded_file_path = ''
+file_path = 'words.txt'
+excluded_file_path = 'excluded.txt'
 
 #function that reads a text file and returns a list of words and stores it in the variable words
 def ReadWords(file_path):
     with open(file_path, 'r') as file:
-        text = file.read().lower()  
+        text = file.read().lower()
 
-    words = re.findall(r'\b\w+\b', text) 
+    words = re.findall(r'\b\w+\b', text)
     return words
 
 #funtion that reads excluded.txt and returns the list of words and stores it in the variable ExcludedWords
 def ReadExcludedWords(file_path):
     with open(file_path, 'r') as file:
         text = file.read().lower()
-    ExcludedWords = re.findall(r'\b\w+\b', text) 
+    ExcludedWords = re.findall(r'\b\w+\b', text)
     return ExcludedWords
 
 # Function to filter out words to be excluded and stores it in the variable filtered_words
 def FilterWords(word_list, exclude_words):
     filtered_words = []
-    for word in word_list: 
-        if word not in exclude_words: 
+    for word in word_list:
+        if word not in exclude_words:
             filtered_words.append(word)
-    return filtered_words 
+    return filtered_words
 
 
 #Main Logic
-WordList = ReadWords(file_path)
-ExcludeList = ReadExcludedWords(excluded_file_path)
-FilteredWords = FilterWords(WordList, ExcludeList)
+if __name__ == "__main__":
+    WordList = ReadWords(file_path)
+    ExcludeList = ReadExcludedWords(excluded_file_path)
+    FilteredWords = FilterWords(WordList, ExcludeList)
 
-# Creates word cloud using using frequency of word counted 
-WordCounts = Counter(FilteredWords)
-wordcloud = WordCloud(
-    width=1000,
-    height=500,
-    background_color='white',
-    max_font_size=100,
-    #colormap= 'winter', 
-).generate_from_frequencies(WordCounts)
+# Creates word cloud using using frequency of word counted
+    WordCounts = Counter(FilteredWords)
+    wordcloud = WordCloud(
+        width=1000,
+        height=500,
+        background_color='white',
+        max_font_size=100,
+        #colormap= 'winter',
+    ).generate_from_frequencies(WordCounts)
 
 # generates the word cloud
-plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off") 
-plt.show()
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
